ecfb3441df96620f5be3e9d4276fbc1484aea2d9
Shai Greenberg <shaigreenbergtm@gmail.com>
2021-07-17 23:54:22 +0300

    adding warnings

--- install_po_rtl_heb_win.py
+++ install_po_rtl_heb_win.py
@@ -1,6 +1,4 @@
 import file_tools as ft
-
-ft.write_files(ft.steam_path_windows(),'hebrew')
-# steam_path = ft.steam_path_windows()
-# tt.write_additions(ft.get_english_additions_text_path(steam_path),'hebrew')
-# tt.write_captions(ft.get_english_caption_text_path(steam_path),ft.get_compiler_path(steam_path),'hebrew')
+answer = input("This is an alpha version. Press y to acknowledge you are responsible for running this.")
+if answer == "y":
+    ft.write_files(ft.steam_path_windows(), 'hebrew')
--- remove_po_rtl_heb_win.py
+++ remove_po_rtl_heb_win.py
@@ -1,3 +1,5 @@
 import file_tools as ft
 
-ft.restore_backup(ft.steam_path_windows(),'hebrew')
+answer = input("This is an alpha version. Press y to acknowledge you are responsible for running this.")
+if answer == "y":
+    ft.restore_backup(ft.steam_path_windows(),'hebrew')
