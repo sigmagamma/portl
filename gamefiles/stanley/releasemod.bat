6415922fb9980c12857c4ca7ad253b3bc2c95d8b
Shai Greenberg <shaigreenbergtm@gmail.com>
2022-08-18 02:12:48 +0300

    small fix to release script

--- gamefiles/stanley/releasemod.bat
+++ gamefiles/stanley/releasemod.bat
@@ -1,5 +1,7 @@
 set token=%1
 set tag=%2
 echo %token% | gh auth login --with-token
-gh release create %tag% --title "The Stanley Parable Hebrew" --notes "See [Installation guide](https://docs.google.com/document/d/1lsCQosS-TDPFxHi6mnZCE653AfrbqW5MhDCbnV-U_Ds/edit?usp=sharing)."
-gh release upload %tag% *.* --clobber
+::gh release create %tag% --title "The Stanley Parable Hebrew" --notes "See [Installation guide](https://docs.google.com/document/d/1lsCQosS-TDPFxHi6mnZCE653AfrbqW5MhDCbnV-U_Ds/edit?usp=sharing)."
+cd gamefiles\stanley\dist
+for /r %%i in (*) do gh release upload --clobber %tag% %%i
+cd ..\..\..
