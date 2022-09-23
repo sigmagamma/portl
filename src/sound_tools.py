52725677f0992776141bc069284e4501d1e41f0b
sigmagamma <shaigreenbergtm@gmail.com>
2022-09-24 00:27:57 +0300

    Adding analysis of sound lengths

---
+++ src/sound_tools.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import wave
+import contextlib
+
+if __name__ == '__main__':
+    for fname in os.listdir(sys.argv[1]):
+        with contextlib.closing(wave.open(os.path.join(sys.argv[1],fname), 'r')) as f:
+            frames = f.getnframes()
+            rate = f.getframerate()
+            duration = round(frames / float(rate),3)
+            print("{},{}".format(fname,duration))
