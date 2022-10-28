f640185cada99ac82a0aa994ec509f1f7b3aa23b
sigmagamma <shaigreenbergtm@gmail.com>
2022-10-28 19:56:43 +0300

    Small fixes

--- gamefiles/blackmesa/packmod_testers.bat
+++ gamefiles/blackmesa/packmod_testers.bat
@@ -1,4 +1,4 @@
 
-set modpath=C:\Steam\steamapps\common\Black Mesa\bms\custom\portl
+set modpath=C:\Program Files (x86)\Steam\steamapps\common\Black Mesa\bms\custom\portl
 ..\..\venv\Scripts\pyinstaller.exe  --clean .\install_black_mesa_heb_win_testers.spec
 ..\..\venv\Scripts\pyinstaller.exe  --clean .\install_black_mesa_heb_win_testers_admin.spec
