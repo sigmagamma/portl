eeb27113c7a9b11b454a7968b64eacf915fe0873
sigmagamma <shaigreenbergtm@gmail.com>
2023-05-26 12:39:45 +0300

    Hindi test

--- src/blackmesa/install_unattended.py
+++ src/blackmesa/install_unattended.py
@@ -8,7 +8,9 @@ def arabic_ltr_install():
 def arabic_rtl_install():
     ft = FileTools("gamefiles/blackmesa/Black Mesa Arabic RTL.json", "uarabic", store="Steam",unattended=True)
     ft.write_files()
-
+def hindi_ltr_install():
+    ft = FileTools("gamefiles/blackmesa/Black Mesa Hindi.json", "hindi", store="Steam",unattended=True)
+    ft.write_files()
 def hebrew_ltr_install():
     ft = FileTools("gamefiles/blackmesa/Black Mesa Hebrew.json", "hebrew", store="Steam",unattended=True)
     ft.write_files()
