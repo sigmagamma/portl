66e8fa0889e225d3b175d972a25881bc684d0dc2
sigmagamma <shaigreenbergtm@gmail.com>
2024-02-21 01:12:06 +0200

    Adding menu files to installation.

--- gamefiles/stanleydemo/reinstallmod_steam_male.bat
+++ gamefiles/stanleydemo/reinstallmod_steam_male.bat
@@ -5,7 +5,7 @@ FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6
     set origpath=%%A %%B\steamapps\common\The Stanley Parable Demo\thestanleyparabledemo\
     set backuppath=%%A %%B\steamapps\common\The Stanley Parable Demo\thestanleyparabledemo_backup\
     )
-for %%I in (cfg materials resource) do robocopy "%backuppath%\%%I" "%origpath%\%%I" /e
+for %%I in (cfg materials resource scripts) do robocopy "%backuppath%\%%I" "%origpath%\%%I" /e
 call gamefiles\stanleydemo\reinstall_generic.bat male
-for %%I in (cfg materials resource) do robocopy "%modpath%\%%I" "%origpath%\%%I" /e
+for %%I in (cfg materials resource scripts) do robocopy "%modpath%\%%I" "%origpath%\%%I" /e
 copy "C:\Program Files (x86)\Steam\steamapps\common\The Stanley Parable\bin\vguimatsurface.dll" "C:\Program Files (x86)\Steam\steamapps\common\The Stanley Parable Demo\bin\vguimatsurface.dll"
