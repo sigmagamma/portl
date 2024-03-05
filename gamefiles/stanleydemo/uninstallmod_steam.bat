cc1ade26d66a59aadb4aaf4b7aedad95d67a472d
sigmagamma <shaigreenbergtm@gmail.com>
2024-03-06 00:30:00 +0200

    Stanley demo backing up sound folders as well

--- gamefiles/stanleydemo/uninstallmod_steam.bat
+++ gamefiles/stanleydemo/uninstallmod_steam.bat
@@ -5,5 +5,5 @@ FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6
     set origpath=%%A %%B\steamapps\common\The Stanley Parable Demo\thestanleyparabledemo\
     set backuppath=%%A %%B\steamapps\common\The Stanley Parable Demo\thestanleyparabledemo_backup\
     )
-for %%I in (cfg materials resource scripts scenes) do robocopy "%backuppath%\%%I" "%origpath%\%%I" /e
+for %%I in (cfg materials resource scripts scenes sound) do robocopy "%backuppath%\%%I" "%origpath%\%%I" /e
 venv\Scripts\python.exe -c "from src.stanleydemo.install_unattended import %store%_uninstall;%store%_uninstall()"
