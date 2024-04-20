6e1ef8591490708bfc59d5bf21732745579a3df0
sigmagamma <shaigreenbergtm@gmail.com>
2024-04-20 18:58:45 +0300

    changing text for Black Mesa necro patch

--- gamefiles/blackmesa/releasemodarabic.bat
+++ gamefiles/blackmesa/releasemodarabic.bat
@@ -1,7 +1,7 @@
 set token=%1
 set tag=%2
 echo %token% | gh auth login --with-token
-::gh release create %tag% --title "Black Mesa Arabic" --notes "See [Installation guide](https://steamcommunity.com/sharedfiles/filedetails/?id=2896432139)."
+::gh release create %tag% --title "Black Mesa Arabic 04.2024" --notes "See [Installation guide](https://steamcommunity.com/sharedfiles/filedetails/?id=2896432139)."
 cd gamefiles\blackmesa\dist
 for /r %%i in (install_black_mesa_a_win_ltr.exe install_black_mesa_a_win_ltr.zip install_black_mesa_a_win_rtl_ex.zip install_black_mesa_a_win_rtl.zip) do gh release upload --clobber %tag% %%i
 cd ..\..\..
