e8b4c5a99e195c3406c5d13b5ab63f1d61ef0838
sigmagamma <shaigreenbergtm@gmail.com>
2025-06-14 16:09:17 +0300

    Black Mesa Hebrew installaer changes

--- gamefiles/blackmesa/packmod_testers.bat
+++ gamefiles/blackmesa/packmod_testers.bat
@@ -1,4 +1,3 @@
 
 set modpath=C:\Program Files (x86)\Steam\steamapps\common\Black Mesa\bms\custom\portl
 ..\..\venv\Scripts\pyinstaller.exe  --clean .\install_black_mesa_heb_win_testers.spec
-..\..\venv\Scripts\pyinstaller.exe  --clean .\install_black_mesa_heb_win_testers_admin.spec
